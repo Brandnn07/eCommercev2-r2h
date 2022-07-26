2433c790cd82c88e498e648876d6388703aa4f67
Brandon Carter <brandcarter4@gmail.com>
2022-07-26 16:47:57 -0400

    heroku commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 const db = mysql.createConnection({
     host: process.env.HOST,
-    port: process.env.DB_PORT,
+    // port: process.env.DB_PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
